aabe3b07055f1b0584cb7bc11b2b15e890da8887
Javier <hjquiroga1@gmail.com>
2022-04-02 10:00:00 -0300

    Clone issues solved5 identation

--- profiles_api/permissions.py
+++ profiles_api/permissions.py
@@ -15,8 +15,8 @@ class UpdateOwnStatus(permissions.BasePermission):
     """Allow users to update their own status"""
 
     def has_object_permission(self, request, view, obj):
-    ""Chek the user is trying to update their own status"""
-    if request.method in permissions.SAFE_METHODS:
-        return True
+        """Chek the user is trying to update their own status"""
+        if request.method in permissions.SAFE_METHODS:
+            return True
 
-    return obj.user_profile.id == request.user.id
+        return obj.user_profile.id == request.user.id
